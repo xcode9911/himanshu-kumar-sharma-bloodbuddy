72f32758eaa9bdb9dc51c6eb7594f99236728607
Himanshu Sharma <hemy@Himanshus-MacBook-Air.local>
2026-01-01 10:13:25 +0545

    US-4: Implement Password Reset API

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -4,6 +4,8 @@ import {
   verifyOTP,
   resendOTP,
   login,
+  requestPasswordReset,
+  resetPassword,
 } from '../controllers/userController.js';
 import catchAsync from '../utils/catchAsync.js';
 
@@ -13,6 +15,8 @@ router.post('/register', catchAsync(register)); //US-1
 router.post('/verify-otp', catchAsync(verifyOTP));//US-2
 router.post('/resend-otp', catchAsync(resendOTP));//US-2
 router.post('/login', catchAsync(login));//US-3
+router.post('/forgot-password', catchAsync(requestPasswordReset));//US-4
+router.post('/reset-password', catchAsync(resetPassword));//US-4
 
 export default router;
 
